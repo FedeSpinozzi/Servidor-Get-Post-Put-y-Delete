2d37d3a51b02bfe769b77d52b3233fdd2a74bf6f
FedeSpinozzi <fede_spinozzi@hotmail.com>
2023-03-16 22:40:26 -0300

    CartCompleto

--- src/router/carts.router.js
+++ src/router/carts.router.js
@@ -4,9 +4,32 @@ import CartManager from '../services/cart.service.js';
 const cartManager = new CartManager('./carts.json');
 const router = Router();
 
-router.get('/', async (req, res) => {
-    const carts = await cartManager.getCarts();
-    res.json({ carts });
+router.get('/:cid', async (req, res) => {
+    try {
+        const { cid } = req.params;
+        const response = await cartManager.getProductsCartById(cid);
+        return res.json({ products: response });
+    } catch (error) {
+        return res.status(401).send(error.message);
+    }
+});
+
+router.post('/', async (req, res) => {
+    const cart = req.body;
+    if(!cart.products){
+        return res.status(400).send("Bad request");
+    }
+    const response = await cartManager.createCart(cart);
+    res.json(response);
+});
+
+router.post('/:cid/product/:pid', async (req, res) => {
+    const { cid, pid } = req.params;
+    if(!cid || !pid){
+        return res.status(400).send("Bad request");
+    }
+    const response = await cartManager.createProductByCart(cid, pid);
+    res.json(response);
 });
 
 export default router;
--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -4,30 +4,73 @@ export default class CartManager {
     constructor(path) {
         this.path = path
     }
-    async getCarts() {
+
+    async getProductsCartById(cid) {
         if (fs.existsSync(this.path)) {
-            const carts = await fs.promises.readFile(this.path, 'utf-8')
-            return JSON.parse(carts)
-        } else {
-            return []
+            const carts = await fs.promises.readFile(this.path, 'utf-8');
+            const cartParsed = JSON.parse(carts);
+            const cart = cartParsed.find((c)=> {
+                return parseInt(cid) === c.id;
+            });
+            if(!cart){
+                throw new Error('cart not found');
+            }
+            return cart.products;
         }
     }
-    //async createCarts(arr) {
-    //    const cartsFile = await this.getCarts()
-    //    const id = this.createId(cartsFile)
-    //    const newCart = { id, ...arr }
-    //    cartsFile.push(newCart)
-    //    await fs.promises.writeFile(this.path, JSON.stringify(cartsFile))
-    //    return newCart
-    //}
-    
-    //async createId(carts) {
-    //    let id
-    //    if (carts.length === 0) {
-    //        id = 1
-    //    } else {
-    //        id = carts[carts.length - 1].id + 1
-    //    }
-    //    return id
-    //}
+
+    async getCarts() {
+        if (fs.existsSync(this.path)) {
+            const carts = await fs.promises.readFile(this.path, 'utf-8');
+            return JSON.parse(carts);
+        } else {
+            return [];
+        }
+    }
+
+    async createProductByCart(cid , pid) {
+        if (fs.existsSync(this.path)) {
+            const carts = await fs.promises.readFile(this.path, 'utf-8');
+            const cartParsed = JSON.parse(carts);
+            let index;
+            const cart = cartParsed.find((c, i)=> {
+                if(parseInt(cid) === c.id){
+                    index = i;
+                    return c;
+                }
+            });
+            if(!cart){
+                throw new Error('cart not found');
+            }
+            let final = cartParsed.splice(index, 1);
+            let products = cart.products;
+            products.push({id: pid, quantity: 1})
+            final.push({
+                products,
+                ...cart
+            });
+            await fs.promises.writeFile(this.path, JSON.stringify(final));
+            return cart.products;
+        }
+    }
+
+    async createCart(obj) {
+        const cartsFile = await this.getCarts();
+        const id = this.createId(cartsFile);
+        const newCart = { id, ...obj };
+        cartsFile.push(newCart);
+        await fs.promises.writeFile(this.path, JSON.stringify(cartsFile));
+        return newCart;
+    }
+
+    createId(carts) {
+        let id
+        if (carts.length === 0) {
+            id = 1;
+        } else {
+            id = carts[carts.length - 1].id + 1;
+        }
+        return id;
+    }
+
 }
